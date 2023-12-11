bbe4b832d390fd27b6975d8251f03fc908a60e6c
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2023-12-11 19:44:10 +0100

    Eslint format requirements satisfied

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -1,10 +1,17 @@
-const RecipeCard = ({ name, description }) => {
+import PropTypes from 'prop-types';
+
+function RecipeCard({ name, description }) {
   return (
     <div>
       <h3>{name}</h3>
       <p>{description}</p>
     </div>
   );
+}
+
+RecipeCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default RecipeCard;
