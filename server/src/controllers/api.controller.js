a0640f2ef5869e4dec6d07c929c8573aaad35656
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2023-12-14 18:31:28 +0100

    Simplify the error handling middleware code

--- server/src/controllers/api.controller.js
+++ server/src/controllers/api.controller.js
@@ -1,10 +1,8 @@
-import errorHandler from "../middlewares/errorHandler.middleware";
-
 // GET /api/
 export const getApi = (req, res, next) => {
   try {
     res.status(200);
   } catch (err) {
-    errorHandler(err, req, res, next);
+    next(err);
   }
 };
