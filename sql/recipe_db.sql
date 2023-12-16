062a167f7c3d734b93c3950d4d220e2e748ad808
MeetAlica <feher.ali.96@gmail.com>
2023-12-16 02:24:11 +0100

    Reorganized table creation into one file, added unique constraint, and changed serial type to uuid

--- sql/recipe_db.sql
+++ sql/recipe_db.sql
@@ -1,4 +1,17 @@
-CREATE DATABASE recipe_db;
-CREATE TABLE recipes (name VARCHAR(255),
-					  description VARCHAR(255),
-					  id SERIAL PRIMARY KEY);
+CREATE DATABASE recipeking_db;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE recipes (
+	id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+	name VARCHAR(255),
+	description VARCHAR(255)
+);
+
+CREATE TABLE users (
+	id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+	username VARCHAR(100) NOT NULL UNIQUE,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	password_hash VARCHAR(100) NOT NULL,
+	registration_date DATE DEFAULT CURRENT_DATE
+);
