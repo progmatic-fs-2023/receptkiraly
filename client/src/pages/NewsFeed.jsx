1eba9f3989b0fca1e3704f10f3db96db08199547
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2023-12-19 20:47:19 +0100

    Started creating style for the NewsFeed article

--- client/src/pages/NewsFeed.jsx
+++ client/src/pages/NewsFeed.jsx
@@ -1,7 +1,15 @@
-import newsArray from '../assets/data/NewsFeed.data';
+import newsArray from '../../public/data/NewsFeed.data';
 
 function NewsFeed() {
-  return newsArray.map((newsItem) => <div>{newsItem.title}</div>);
+  return newsArray.map((newsItem) => (
+    <article>
+      <h6>{newsItem.columnName}</h6>
+      <h3>{newsItem.title}</h3>
+      <p>{newsItem.headline}</p>
+      <img src={`../../public/data/images/${newsItem.authorImage}`} alt="" />
+      <br />
+      <hr />
+    </article>
+  ));
 }
-
 export default NewsFeed;
