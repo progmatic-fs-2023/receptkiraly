e83ba91b802c80bfc6a862e4d62c6605d42b1349
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2023-12-20 17:44:44 +0100

    Added styles to newsfeed using Tailwind

--- client/src/pages/NewsFeed.jsx
+++ client/src/pages/NewsFeed.jsx
@@ -1,15 +1,23 @@
-import newsArray from '../../public/data/NewsFeed.data';
+import newsArray from '/data/NewsFeed.data.js';
 
 function NewsFeed() {
   return newsArray.map((newsItem) => (
-    <article>
-      <h6>{newsItem.columnName}</h6>
-      <h3>{newsItem.title}</h3>
-      <p>{newsItem.headline}</p>
-      <img src={`../../public/data/images/${newsItem.authorImage}`} alt="" />
+    <div className="w-auto md:w-4/5	 lg:w-3/5	xl:w-2/5 2xl:w-2/5	">
       <br />
+      <article className="flex-row flex justify-between	">
+        <div className="flex-col">
+          <h6 className="text-1xl font-medium text-gray-500 dark:text-white">
+            {newsItem.columnName}
+          </h6>
+          <h3 className="text-2xl font-medium text-gray-900 dark:text-white">{newsItem.title}</h3>
+          <p>{newsItem.headline}</p>
+        </div>
+        <div className="flex flex-col justify-end">
+          <img className="" src={`images/${newsItem.authorImage}`} alt="" />
+        </div>
+      </article>
       <hr />
-    </article>
+    </div>
   ));
 }
 export default NewsFeed;
