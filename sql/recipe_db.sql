e34fbab5dd4561b5358acf2b2269fc2d5a5aff27
MeetAlica <feher.ali.96@gmail.com>
2023-12-23 02:28:32 +0100

    Organizing SQL codes into one file, changed users table to handle unique username and email, and added new users_recipes switch table.

--- sql/recipe_db.sql
+++ sql/recipe_db.sql
@@ -1,4 +1,26 @@
 CREATE DATABASE recipe_db;
-CREATE TABLE recipes (name VARCHAR(255),
-					  description VARCHAR(255),
-					  id SERIAL PRIMARY KEY);
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE recipes (
+	id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	description VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE users (
+	id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+	username VARCHAR(100) NOT NULL UNIQUE,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	password_hash VARCHAR(255) NOT NULL,
+	registration_date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE users_recipes (
+	user_id UUID,
+	recipe_id UUID,
+	PRIMARY KEY (user_id, recipe_id),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+);
