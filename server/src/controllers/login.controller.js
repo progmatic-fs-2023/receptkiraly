1a8cf32c7fa35726c80551147082276820e21b9b
kotaizsaklin <kotai.zsaklin@gmail.com>
2023-12-29 11:12:23 +0100

    Bejelentkezés után profil oldalra váltás, token alapú bejelentkezés

--- server/src/controllers/login.controller.js
+++ server/src/controllers/login.controller.js
@@ -3,10 +3,11 @@ import * as services from '../services/login.services';
 export const login = async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await services.loginUser(username, password);
+    const result = await services.loginUser(username, password);
 
-    if (user) {
-      res.status(200).json({ message: 'Login successful', user });
+    if (result) {
+      const { user, token } = result;
+      res.status(200).json({ message: 'Login successful', user, token });
     } else {
       res.status(401).json({ message: 'Invalid username or password' });
     }
--- server/src/services/login.services.js
+++ server/src/services/login.services.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import * as db from './db.service';
 
 export const loginUser = async (username, password) => {
@@ -5,5 +6,12 @@ export const loginUser = async (username, password) => {
     username,
     password,
   ]);
-  return result.rows[0];
+  const user = result.rows[0];
+
+  if(user){
+    const token = jwt.sign({ userId: user.user_id, username: user.username }, 'secret_key');
+    return { user, token };
+  }
+
+  return null;
 };
