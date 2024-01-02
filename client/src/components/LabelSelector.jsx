f007214dc8e11bcc5d6e36d854444ed2476aeb80
TOMI <tamas.kovacs1992@gmail.com>
2024-01-02 20:58:18 +0100

    Labelselector létrehozása

---
+++ client/src/components/LabelSelector.jsx
@@ -0,0 +1,93 @@
+import Button from './Button';
+
+function labelSelector() {
+  return (
+    <div>
+      <h3 className="mb-4 font-semibold text-gray-900 dark:text-white ">Search</h3>
+      <ul className=" list-style-type: none w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+        <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600  ">
+          <div className="flex items-center ps-3">
+            <label
+              htmlFor="vegetarian-checkbox"
+              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              <input
+                id="vegetarian-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              Vegetarian
+            </label>
+          </div>
+        </li>
+        <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+          <div className="flex items-center ps-3">
+            <label
+              htmlFor="vegan-checkbox"
+              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              <input
+                id="vegan-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              Vegan
+            </label>
+          </div>
+        </li>
+        <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+          <div className="flex items-center ps-3">
+            <label
+              htmlFor="glutenfree-checkbox"
+              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              <input
+                id="glutenfree-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              Gluten free
+            </label>
+          </div>
+        </li>
+        <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+          <div className="flex items-center ps-3">
+            <label
+              htmlFor="lactosefree-checkbox"
+              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              <input
+                id="lactosefree-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              Lactose free
+            </label>
+          </div>
+        </li>
+        <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+          <div className="flex items-center ps-3">
+            <label
+              htmlFor="sugarfree-checkbox"
+              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              <input
+                id="sugarfree-checkbox"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              Sugar free
+            </label>
+          </div>
+        </li>
+      </ul>
+      <Button text="Search" />
+    </div>
+  );
+}
+export default labelSelector;
