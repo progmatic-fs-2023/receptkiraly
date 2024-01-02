48745b5f41a1cc245e5724fbec48df41ee062d45
TOMI <tamas.kovacs1992@gmail.com>
2024-01-02 20:11:00 +0100

    Gomb komponens létrehozása

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import PropTypes, { func } from 'prop-types';
+
+function handleClick() {
+  return;
+}
+function Button(props) {
+  return <button onClick={handleClick}>{props.text}</button>;
+}
+
+ButtonProject.propTypes = {
+  props: PropTypes.string.isRequired,
+};
+export default Button;
