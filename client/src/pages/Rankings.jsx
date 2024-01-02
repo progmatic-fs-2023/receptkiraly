ec02d5728a7bc946181a08f9de936b015e028dac
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-02 01:17:38 +0100

    Adjust the rankings page

--- client/src/pages/Rankings.jsx
+++ client/src/pages/Rankings.jsx
@@ -1,9 +1,28 @@
+import RankingList from '../components/RankingList';
+
 function Rankings() {
   return (
-    <>
-      <h1>Rankings</h1>
-      <h1>Rankings</h1>
-    </>
+    <div className="mb-16 flex flex-wrap">
+      <RankingList />
+      <aside className="w-full shrink-0 grow-0 basis-auto lg:w-6/12 lg:pl-6">
+        <h1>Your rank</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed libero reiciendis, porro
+          architecto aperiam accusantium. Ducimus, natus quaerat accusantium asperiores corporis,
+          dicta autem fugit esse impedit eos facilis? Commodi, officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed libero reiciendis, porro
+          architecto aperiam accusantium. Ducimus, natus quaerat accusantium asperiores corporis,
+          dicta autem fugit esse impedit eos facilis? Commodi, officia.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed libero reiciendis, porro
+          architecto aperiam accusantium. Ducimus, natus quaerat accusantium asperiores corporis,
+          dicta autem fugit esse impedit eos facilis? Commodi, officia.
+        </p>
+      </aside>
+    </div>
   );
 }
 
