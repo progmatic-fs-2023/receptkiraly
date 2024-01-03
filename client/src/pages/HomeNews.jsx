0e74b1e68d28e167cd5d8502eb6be2cbca8ba940
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-01-03 19:10:18 +0100

    Imported battlecard component to homepage

--- client/src/pages/HomeNews.jsx
+++ client/src/pages/HomeNews.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { API_URL } from '../constants';
 import NewsCarousel from '../components/NewsCarousel';
+import BattleCard from '../components/BattleCard';
 
 function HomeNews() {
   const [isConnect, setIsConnect] = useState(false);
@@ -20,6 +21,12 @@ function HomeNews() {
         </li>
       </ul>
       <NewsCarousel />
+      <BattleCard
+        imageLeftUrl="https://stordfkenticomedia.blob.core.windows.net/df-us/rms/media/recipemediafiles/recipes/retail/x17/16714-birthday-cake-600x600.jpg?ext=.jpg"
+        imageRightUrl="https://i0.wp.com/www.onceuponachef.com/images/2020/09/Sprinkle-Funfetti-Cake-scaled.jpg?resize=760%2C896&ssl=1"
+        title="Cake of 2024"
+        description="Vote for your favorite"
+      />
     </div>
   );
 }
