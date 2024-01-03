685f91531c585f7f6b0de03219d44be828e12913
TOMI <tamas.kovacs1992@gmail.com>
2024-01-03 19:05:45 +0100

    Recipecard kibővítése

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types';
 
-function RecipeCard({ name, description }) {
+function RecipeCard({ name, description, imgUrl }) {
   return (
     <div>
+      {imgUrl ? <img src={imgUrl} alt="" /> : null}
       <h3>{name}</h3>
-      <p>{description}</p>
+      {description ? <p>{description}</p> : null}
     </div>
   );
 }
 
 RecipeCard.propTypes = {
   name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  imgUrl: PropTypes.string,
 };
-
+RecipeCard.defaultProps = {
+  description: '',
+  imgUrl: '',
+};
 export default RecipeCard;
