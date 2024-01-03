72f7508824f5eed9c2ba9ff758006393e1c8ea92
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-01-03 19:09:00 +0100

    Added battlecard component

--- client/src/components/BattleCard.jsx
+++ client/src/components/BattleCard.jsx
@@ -1,30 +1,36 @@
-function BattleCard() {
+import PropTypes from 'prop-types';
+
+function BattleCard({ imageLeftUrl, imageRightUrl, title, description }) {
   return (
     <a
-      href="#"
-      class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      href="/"
+      className="flex flex-col items-between bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 justify-between"
     >
       <img
-        class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-        src="/docs/images/blog/image-4.jpg"
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        src={imageLeftUrl}
         alt=""
-      ></img>
-      <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
+      />
+      <div className="flex flex-col justify-center p-4 leading-normal text-center">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
         </h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
-          chronological order.
-        </p>
-        <img
-          class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-          src="/docs/images/blog/image-4.jpg"
-          alt=""
-        ></img>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
       </div>
+      <img
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-r-lg"
+        src={imageRightUrl}
+        alt=""
+      />
     </a>
   );
 }
+
+BattleCard.propTypes = {
+  imageLeftUrl: PropTypes.string.isRequired,
+  imageRightUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default BattleCard;
