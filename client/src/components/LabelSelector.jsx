b66ff43b2c17c5dd3946018a144d267552e5cda0
TOMI <tamas.kovacs1992@gmail.com>
2024-01-04 19:18:02 +0100

    A kereső oldal frontendjének elkészítése és elemeinek összeillesztése

--- client/src/components/LabelSelector.jsx
+++ client/src/components/LabelSelector.jsx
@@ -1,7 +1,7 @@
 function labelSelector() {
   return (
     <div>
-      <h3 className="mb-4 font-semibold text-gray-900 dark:text-white ">Search</h3>
+      <h3 className="mb-4 font-semibold text-gray-900 dark:text-white text-center ">Search</h3>
       <ul className=" list-style-type: none w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
         <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600  ">
           <div className="flex items-center ps-3">
--- client/src/pages/SearchRecipes.jsx
+++ client/src/pages/SearchRecipes.jsx
@@ -1,4 +1,20 @@
+import LabelSelector from '../components/LabelSelector';
+import Button from '../components/Button';
+import SearchResults from '../components/SearchResult';
+
 function SearchRecipes() {
-  return 'SearchRecipes';
+  return (
+    <div className="flex">
+      <div>
+        <LabelSelector />
+        <div className="text-center">
+          <Button text="Search" />
+        </div>
+      </div>
+      <div>
+        <SearchResults />
+      </div>
+    </div>
+  );
 }
 export default SearchRecipes;
