98c681a2dd8f8776cfaef47a55558b233f337902
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-04 20:31:45 +0100

    This component render the list of query items.

---
+++ client/src/components/RankingQueryResultList.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import RankingQueryResultItem from './RankingQueryResultItem';
+
+function RankingQueryResultList({ query, queryValue }) {
+  const actualQueryValue = queryValue;
+  return query.map((element) => (
+    <ul key={element.key}>
+      <RankingQueryResultItem
+        queryValue={actualQueryValue}
+        key={element.key}
+        img={element.img}
+        name={element.name}
+        description={element.description}
+        score={element.score}
+        reviews={element.reviews}
+        rank={element.rank}
+      />
+    </ul>
+  ));
+}
+
+RankingQueryResultList.propTypes = {
+  query: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.number.isRequired,
+      img: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      rank: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  queryValue: PropTypes.string.isRequired,
+};
+export default RankingQueryResultList;
