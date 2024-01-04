018eeb94954ce0e3ccaa81c2f389ba8735c22b9e
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-04 20:29:22 +0100

    Finalize the ranking list component, with dummy data.

--- client/src/components/RankingList.jsx
+++ client/src/components/RankingList.jsx
@@ -1,19 +1,37 @@
 import { useState } from 'react';
 import RankingListQueryButtons from './RankingListQueryButtons';
+import RankingQueryResultList from './RankingQueryResultList';
+import mockRankings from '../mockRankings';
 
 function RankingList() {
-  const [queryRanking, setQueryRankings] = useState('valami');
+  const [queryRanking, setQueryRankings] = useState([]);
+  const [actualQueryValue, setActualQueryValue] = useState('');
+
+  const mockData = mockRankings;
 
   function handleRankingQueryButton(string) {
-    setQueryRankings(string);
+    setActualQueryValue(string);
+    setQueryRankings(mockData[string]);
+
+    // Ez itt lenne a fetch ha majd megírjuk a backendet hozzá.
+    /*
+    try {
+      const response = await fetch(`http://localhost:3000/${string}`);
+      const data = await response.json();
+      setActualQueryValue(string);
+      setQueryRankings(data);
+    } catch (error) {
+      console.error('Error has accured:', error);
+    }
+
+    */
   }
 
   return (
-    <section className="mb-6 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-6/12 lg:pr-6">
+    <section>
       <h1>Rankings</h1>
-      <RankingListQueryButtons queryCallback={handleRankingQueryButton} />
-      <p>{queryRanking}</p>
-      
+      <RankingListQueryButtons queryCallback={(string) => handleRankingQueryButton(string)} />
+      <RankingQueryResultList query={queryRanking} queryValue={actualQueryValue} />
     </section>
   );
 }
