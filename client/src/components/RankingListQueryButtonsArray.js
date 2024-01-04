69fc197b6fdfa6b41bb8acffd492aa1236bc9ed6
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-04 20:28:25 +0100

    Create the buttons array, what can use for queries the ranking lists.

--- client/src/components/RankingListQueryButtonsArray.js
+++ client/src/components/RankingListQueryButtonsArray.js
@@ -2,7 +2,7 @@ const queryButtons = [
   { key: 1, label: 'Visual', value: 'Visual' },
   { key: 2, label: 'Taste', value: 'Taste' },
   { key: 3, label: 'Cooked', value: 'Cooked' },
-  { key: 4, label: 'Battle wins', value: 'Battle wins' },
+  { key: 4, label: 'Battle wins', value: 'Battle_wins' },
   { key: 5, label: 'Overall', value: 'Overall' },
 ];
 
