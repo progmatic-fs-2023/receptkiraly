5756824f9182f2050c82351ab932b6d504154463
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-04 20:27:32 +0100

    Add some basic style for rankings page

--- client/src/pages/Rankings.jsx
+++ client/src/pages/Rankings.jsx
@@ -3,24 +3,12 @@ import RankingList from '../components/RankingList';
 function Rankings() {
   return (
     <div className="mb-16 flex flex-wrap">
-      <RankingList />
-      <aside className="w-full shrink-0 grow-0 basis-auto lg:w-6/12 lg:pl-6">
+      <RankingList className="w-full lg:w-4/5" />
+      <aside className="w-full lg:w-1/5 lg:pl-6">
         <h1>Your rank</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed libero reiciendis, porro
-          architecto aperiam accusantium. Ducimus, natus quaerat accusantium asperiores corporis,
-          dicta autem fugit esse impedit eos facilis? Commodi, officia.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed libero reiciendis, porro
-          architecto aperiam accusantium. Ducimus, natus quaerat accusantium asperiores corporis,
-          dicta autem fugit esse impedit eos facilis? Commodi, officia.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed libero reiciendis, porro
-          architecto aperiam accusantium. Ducimus, natus quaerat accusantium asperiores corporis,
-          dicta autem fugit esse impedit eos facilis? Commodi, officia.
-        </p>
+        <p>Mindenféle badge és jóság</p>
+        <p>User adatok és mindenféle jóság és királyság</p>
+        <p>Legyen itt is valami, miért ne egyéb cuccok.</p>
       </aside>
     </div>
   );
