7ce85caaff0201dfbcecdebd4bd0414b0b2dafb6
kotaizsaklin <kotai.zsaklin@gmail.com>
2024-01-06 21:26:24 +0100

    Formázás tailwindel 1.0

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,17 +1,37 @@
 function Banner() {
   return (
-    <div className="flex items-center justify-between bg-indigo-500 p-4">
+    <div className="flex items-center justify-between p-4">
+      <div className="flex flex-row">
       <div className="flex items-center">
-        <h1 className="text-2xl font-bold text-white ml-2">Recept Király</h1>
+        <h1 className="text-2xl font-bold text-black ml-2 mr-2">Recipe King</h1>
+      </div>  
+      <div className="flex items-center">
         <input
           type="text"
-          placeholder="Keresés..."
-          className="px-2 py-1 mr-2 rounded border"
+          placeholder="Search..."
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
+        <button
+          type="button"
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        >
+          Search
+        </button>
+      </div>
       </div>
       <div className="flex items-center">
-        <button type="button" className="px-3 py-1 bg-white text-indigo-500 rounded">Gomb 1</button>
-        <button type="button" className="px-3 py-1 bg-white text-indigo-500 rounded ml-2">Gomb 2</button>
+        <button
+          type="button"
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        >
+          Button 1
+        </button>
+        <button
+          type="button"
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        >
+          Button 2
+        </button>
       </div>
     </div>
   );
