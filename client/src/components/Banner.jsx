c1e2ab37fee76a017eac9337910c8aa33406226a
kotaizsaklin <kotai.zsaklin@gmail.com>
2024-01-06 17:53:19 +0100

    Fejléc kialakítása: logó, search bar, 2 gomb

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,9 +1,17 @@
 function Banner() {
   return (
-    <div className="relative">
-      <img src="banner.png" alt="Banner" className="banner-image w-full h-64 object-cover" />
-      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-        <h1 className="text-7xl font-bold text-white">Recept Király</h1>
+    <div className="flex items-center justify-between bg-indigo-500 p-4">
+      <div className="flex items-center">
+        <h1 className="text-2xl font-bold text-white ml-2">Recept Király</h1>
+        <input
+          type="text"
+          placeholder="Keresés..."
+          className="px-2 py-1 mr-2 rounded border"
+        />
+      </div>
+      <div className="flex items-center">
+        <button type="button" className="px-3 py-1 bg-white text-indigo-500 rounded">Gomb 1</button>
+        <button type="button" className="px-3 py-1 bg-white text-indigo-500 rounded ml-2">Gomb 2</button>
       </div>
     </div>
   );
