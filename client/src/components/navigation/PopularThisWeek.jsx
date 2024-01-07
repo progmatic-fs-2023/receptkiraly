3ce2a077930e783667d47bf3eb1b5f1c0784ca86
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-07 23:18:29 +0100

    Create the PopularThisWeek component.

---
+++ client/src/components/navigation/PopularThisWeek.jsx
@@ -0,0 +1,38 @@
+function PopularThisWeek() {
+  return (
+    <div className="bg-[#eee] pl-12 py-12 relative after:block after:bg-[#eee] after:absolute after:left-full after:h-full after:top-0 after:w-screen">
+      <p className="block text-xs uppercase text-body/60">Popular This Week</p>
+      <div className="flex mt-8 space-x-6">
+        <a
+          className="hover:text-primary text-body max-w-[204px]"
+          href="https://foodnetwork.co.uk/recipes/peppadew-bbq-chicken-slider"
+        >
+          <img
+            srcSet="https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_2480_1395.webp 2480w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_2074_1167.webp 2074w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_1735_976.webp 1735w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_1452_817.webp 1452w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_1215_683.webp 1215w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_1016_572.webp 1016w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_850_478.webp 850w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_711_400.webp 711w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11056/responsive-images/foodnetwork-image-52fd6ee4-cf24-4c86-a4e1-3924d83ae941___landscape_595_335.webp 595w"
+            loading="lazy"
+            width="1240"
+            height="698"
+            className="mb-4 rounded"
+            alt=""
+          />
+          <p className="capitalize font-medium text-sm">PEPPADEW® BBQ Chicken Slider</p>
+        </a>
+        <a
+          className="hover:text-primary text-body max-w-[204px]"
+          href="https://foodnetwork.co.uk/recipes/argentinean-barbecued-steak"
+        >
+          <img
+            srcSet="https://d2vsf1hynzxim7.cloudfront.net/production/media/11377/responsive-images/foodnetwork-image-d521b113-db29-4276-898e-5f7535f9d130___landscape_908_510.webp 908w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11377/responsive-images/foodnetwork-image-d521b113-db29-4276-898e-5f7535f9d130___landscape_759_426.webp 759w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11377/responsive-images/foodnetwork-image-d521b113-db29-4276-898e-5f7535f9d130___landscape_635_357.webp 635w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11377/responsive-images/foodnetwork-image-d521b113-db29-4276-898e-5f7535f9d130___landscape_531_298.webp 531w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11377/responsive-images/foodnetwork-image-d521b113-db29-4276-898e-5f7535f9d130___landscape_444_249.webp 444w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11377/responsive-images/foodnetwork-image-d521b113-db29-4276-898e-5f7535f9d130___landscape_372_209.webp 372w, https://d2vsf1hynzxim7.cloudfront.net/production/media/11377/responsive-images/foodnetwork-image-d521b113-db29-4276-898e-5f7535f9d130___landscape_311_175.webp 311w"
+            loading="lazy"
+            width="1240"
+            height="698"
+            className="mb-4 rounded"
+            alt=""
+          />
+          <p className="capitalize font-medium text-sm">Argentinean Barbecued Steak</p>
+        </a>
+      </div>
+    </div>
+  );
+}
+export default PopularThisWeek;
