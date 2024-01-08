1925c0815c42e429e464e7adc601b08e070a3447
MeetAlica <feher.ali.96@gmail.com>
2024-01-08 16:08:08 +0100

    Changed Icon component for handle edit mode

--- client/src/components/Icon.jsx
+++ client/src/components/Icon.jsx
@@ -1,17 +1,26 @@
 import PropTypes from 'prop-types';
 
-function Icon({ imgUrl, text }) {
+function Icon({ imgUrl, text, isLoggedIn }) {
+  
   return (
     <div className="flex items-center opacity-60">
       <img src={imgUrl} className="h-[16px] w-[16px] grow-0 h-auto w-auto" alt="" />
-      <span className="text-[12px] leading-[18px] ml-[8px] mr-[16px] h5 uppercase whitespace-nowrap">
-        {text}
-      </span>
+      {isLoggedIn ? (
+        <input
+          type="text"
+          className="text-[12px] leading-[18px] ml-[8px] mr-[16px] h5 uppercase whitespace-nowrap"
+        />
+      ) : (
+        <span className="text-[12px] leading-[18px] ml-[8px] mr-[16px] h5 uppercase whitespace-nowrap">
+          {text}
+        </span>
+      )}
     </div>
   );
 }
 Icon.propTypes = {
   imgUrl: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
 };
 export default Icon;
