0a42235c6569c2681ca04f473fb31f1175e22f3b
MeetAlica <feher.ali.96@gmail.com>
2024-01-08 14:08:21 +0100

    Created RecipeText component, edit mode included (need a prop called isLoggedIn, value is boolean)

---
+++ client/src/components/RecipeText.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function RecipeText({ isLoggedIn }) {
+  const { recipeText, setRecipeText } = useState('');
+
+  const handleRecipeTextChange = (event) => {
+    setRecipeText(event.target.value);
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <textarea
+          value={recipeText}
+          onChange={handleRecipeTextChange}
+          placeholder="Say something about this recipe!"
+          rows={8}
+        />
+      ) : (
+        <p>{recipeText}</p>
+      )}
+    </div>
+  );
+}
+
+RecipeText.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+};
+
+export default RecipeText;
