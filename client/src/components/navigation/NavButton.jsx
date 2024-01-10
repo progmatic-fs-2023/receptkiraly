d7f11d7afc0b45f3704dcfe994ba2faa2e9d471d
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-10 20:00:13 +0100

    Add NavButton component

---
+++ client/src/components/navigation/NavButton.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+function ExploreAllButton({ text }) {
+  return (
+    <div className="mt-6">
+      <a
+        className="block border border-primary font-semibold hover:bg-primary hover:bg-orange-400 hover:text-white py-3 px-8 rounded sm:inline-block text-primary text-sm text-center uppercase"
+        target="_self"
+        title=""
+        href="https://foodnetwork.co.uk/recipes"
+      >
+        <span>{text}</span>
+      </a>
+    </div>
+  );
+}
+
+ExploreAllButton.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default ExploreAllButton;
