bb8ffb3c04f6ff3dfc4bd77771555ebfae3d811e
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-10 20:18:58 +0100

    Change the color of the button.

--- client/src/components/navigation/NavButton.jsx
+++ client/src/components/navigation/NavButton.jsx
@@ -4,7 +4,7 @@ function ExploreAllButton({ text }) {
   return (
     <div className="mt-6">
       <a
-        className="block border border-primary font-semibold hover:bg-primary hover:bg-orange-400 hover:text-white py-3 px-8 rounded sm:inline-block text-primary text-sm text-center uppercase"
+        className="block border-2 border-orange-400 font-semibold hover:bg-primary hover:bg-orange-400 hover:text-white py-3 px-8 rounded sm:inline-block text-orange-400 text-sm text-center uppercase"
         target="_self"
         title=""
         href="http://localhost:5173/searchrecipes"
