bd4295af752eaa347d86ceeca67c6d59ae0de67f
kotaizsaklin <kotai.zsaklin@gmail.com>
2024-01-11 20:02:20 +0100

    Előkészülés: Felesleges fájlok és elemek törlése/kiürítése.

--- client/src/pages/SearchRecipes.jsx
+++ client/src/pages/SearchRecipes.jsx
@@ -1,27 +1,6 @@
-import LabelSelector from '../components/LabelSelector';
-import Button from '../components/Button';
-import SearchResultRecepies from '../components/SearchResultRecepies';
-
 function SearchRecipes() {
   return (
-    <div className="flex">
-      <div>
-        <LabelSelector />
-        <div className="text-center">
-          <Button text="Search" />
-        </div>
-      </div>
-      <section className="text-gray-600 body-font ">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            <SearchResultRecepies />
-            <SearchResultRecepies />
-            <SearchResultRecepies />
-            <SearchResultRecepies />
-          </div>
-        </div>
-      </section>
-    </div>
+    <div />
   );
 }
 export default SearchRecipes;
