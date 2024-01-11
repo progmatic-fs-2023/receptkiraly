504ca314066a6bb9e7a80ae6c8b0f93f01f18731
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-11 23:45:38 +0100

    Redesign the select for recipe categories.

--- client/src/components/RecipeCategory.jsx
+++ client/src/components/RecipeCategory.jsx
@@ -13,12 +13,36 @@ function RecipeCategory({ editMode, category, setCategory }) {
     { value: 'Soup', label: 'Soup' },
   ];
 
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      border: '3px solid #ff8811',
+      fontSize: '20px',
+      fontWeight: 'bold',
+
+      boxShadow: state.isFocused ? '0 0 0 1px #5a8dee' : 'none',
+      '&:hover': {
+        boxShadow: '0 0 0 1px #ff8822',
+        fontSize: '20px',
+      },
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: 'black',
+    }),
+  };
+
   const selectedCategory = categories.find((c) => c.value === category);
 
   return (
-    <div>
+    <div className="w-1/6">
       {editMode ? (
-        <Select options={categories} value={selectedCategory} onChange={onValueChange} />
+        <Select
+          options={categories}
+          value={selectedCategory}
+          onChange={onValueChange}
+          styles={customStyles}
+        />
       ) : (
         <div>{category}</div>
       )}
