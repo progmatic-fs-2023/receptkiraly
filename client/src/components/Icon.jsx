67543270913e810f502c6212a686d01f51600fb0
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-11 23:43:17 +0100

    Change the size of the icons and the text area.

--- client/src/components/Icon.jsx
+++ client/src/components/Icon.jsx
@@ -1,14 +1,13 @@
 import PropTypes from 'prop-types';
 
 function Icon({ imgUrl, text, editMode }) {
-  
   return (
     <div className="flex items-center opacity-60">
-      <img src={imgUrl} className="h-[16px] w-[16px] grow-0 h-auto w-auto" alt="" />
+      <img src={imgUrl} className="h-[31px] w-[31px] grow-0 h-auto w-auto" alt="" />
       {editMode ? (
         <input
           type="text"
-          className="text-[12px] leading-[18px] ml-[8px] mr-[16px] h5 uppercase whitespace-nowrap"
+          className="pl-2 border-solid border-4 border-orange-500 text-[25px] leading-[18px] ml-[10px] mr-[20px] h5 uppercase whitespace-nowrap m-2"
         />
       ) : (
         <span className="text-[12px] leading-[18px] ml-[8px] mr-[16px] h5 uppercase whitespace-nowrap">
