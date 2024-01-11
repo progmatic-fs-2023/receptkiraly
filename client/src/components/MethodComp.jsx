de6fb2837bfdb364344cf1e853617b3e03b6e9ca
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-11 23:45:01 +0100

    A bit new design for the textarea.

--- client/src/components/MethodComp.jsx
+++ client/src/components/MethodComp.jsx
@@ -10,12 +10,20 @@ function MethodComp({ editMode }) {
 
   return (
     <div className="p-8">
-      <h1 className="flex mx-auto justify-center sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Method:</h1>
+      <h1 className="flex mx-auto justify-center sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+        Method:
+      </h1>
       <div>
         {editMode ? (
-          <textarea className="flex mx-auto bg-amber-300 text-trueGray-900" value={description} onChange={handleDescriptionChange} rows={12} cols={60} />
+          <textarea
+            className="flex mx-auto bg-orange-300 text-black p-2 text-xl"
+            value={description}
+            onChange={handleDescriptionChange}
+            rows={12}
+            cols={60}
+          />
         ) : (
-          <p className="flex mx-auto bg-amber-300 text-trueGray-900">{description}</p>
+          <p className="flex mx-auto bg-amber-300 text-black">{description}</p>
         )}
       </div>
     </div>
