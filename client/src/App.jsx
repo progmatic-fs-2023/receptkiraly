82103538eada57e406a55ae08986c25803405001
kotaizsaklin <kotai.zsaklin@gmail.com>
2024-01-13 11:42:31 +0100

    Reszponzív, eltűnik kis méreten

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,8 +29,8 @@ function App() {
           <NavigationBar />
         </div>
         </header>
-        <img src="/images/meal.svg" alt="Meal Icon" className="absolute left-0 top-0 z-0"/>
-        <img src="/images/stirring.svg" alt="Meal Icon" className="absolute right-0 transform scale-y-[-1] top-0 z-0"/>
+        <img src="/images/meal.svg" alt="Meal Icon" className="absolute left-0 top-0 z-0 hidden sm:block"/>
+        <img src="/images/stirring.svg" alt="Meal Icon" className="absolute right-0 transform scale-y-[-1] top-0 z-0 hidden sm:block"/>
         <main className="container mx-auto my-2">
         <ErrorBoundary FallbackComponent={Fallback}>
             <Routes>
