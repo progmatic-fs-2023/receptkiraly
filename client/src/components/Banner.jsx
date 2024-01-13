ba3a75aea62715b593165e9495faa0d00fb3c9bf
kotaizsaklin <kotai.zsaklin@gmail.com>
2024-01-13 16:42:14 +0100

    Reszponzív részek igazítása

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -31,43 +31,19 @@ function Banner() {
   };
 
   return (
-    <div className="container mx-auto flex flex-col sm:flex-col md:flex-row justify-between py-6 sm:h-28 relative z-20">
-      <div className="sm:hidden">
-        <div className="flex justify-center mb-4">
-          <Link to="/" className="focus:outline-none">
-            <img src={Logo} alt="Recipe King" className="logo" />
-          </Link>
-        </div>
-        <div className="flex justify-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            onKeyDown={handleKeyDown}
-            className="w-96 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-1"
-          />
-          <button
-            type="button"
-            onClick={handleSearch}
-            className="bg-orange-500 text-white px-4 py-2 rounded focus:outline-none"
-          >
-            Search
-          </button>
-        </div>
-      </div>
-      <div className="hidden sm:flex w-full md:w-3/4 justify-around sm:items-end">
+    <div className="container mx-auto flex flex-wrap flex-row sm:justify-between justify-center py-6 relative z-20">
+      <div className="flex flex-wrap flex-row w-full md:w-3/4 justify-around items-center">
         <Link to="/" className="focus:outline-none">
           <img src={Logo} alt="Recipe King" className="logo" />
         </Link>
-        <div className="flex items-end mx-6">
+        <div className="flex items-center mx-6">
           <input
             type="text"
             placeholder="Search..."
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
             onKeyDown={handleKeyDown}
-            className="w-96 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-1"
+            className="w-96 shadow-sm appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mx-1"
           />
           <button
             type="button"
