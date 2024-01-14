c10ffaa51d0349e2958b66080d713e6cd00c0cc7
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-01-14 13:34:42 +0100

    Added fetch and new states to Profile component, the display is now using the fetched data

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,12 +6,20 @@ import RecipeGrid from '../components/RecipeGrid';
 import RecipeCard from '../components/RecipeCard';
 import Modal from '../components/Modal';
 import DetailedRecipe from '../components/DetailedRecipe';
+import convertIsoTimestampToDate from '../helpers';
 
 function Profile() {
   const [isConnect, setIsConnect] = useState(false);
   const [creatingNewRecipe, setCreatingNewRecipe] = useState(false);
   const [myRecipes, setMyRecipes] = useState([1, 2, 3, 4]);
-  const userID = 5;
+  const [userData, setUserData] = useState({
+    user_id: 'dummy1',
+    user_username: 'dummy',
+    user_email: 'john@example.com',
+    user_password_hash: 'hashed_password_123',
+    user_registration_date: '2024-01-13T23:00:00.000Z',
+  });
+  const userID = '5703d862-c54c-47dc-bfc7-911e242ca24a';
   useEffect(() => {
     fetch(`${API_URL}`).then((response) => {
       if (response.ok) setIsConnect(true);
@@ -23,6 +31,14 @@ function Profile() {
       })
       .then((data) => {
         setMyRecipes(data.ids);
+      });
+    fetch(`${API_URL}/user/${userID}`)
+      .then((response) => {
+        if (!response.ok) throw new Error('User cannot be fetched');
+        return response.json();
+      })
+      .then((data) => {
+        setUserData(data);
       });
   }, []);
 
@@ -59,6 +75,38 @@ function Profile() {
       serves: 4,
       name: 'Strawberry Smoothie',
     },
+    {
+      id: 7,
+      imgUrl: '../images/food/scrambled_eggs.png',
+      minutes: 30,
+      difficulty: 'Easy',
+      serves: 4,
+      name: 'Scrambled Eggs',
+    },
+    {
+      id: 8,
+      imgUrl: '../images/food/spaghetti_bolognese.png',
+      minutes: 45,
+      difficulty: 'Easy',
+      serves: 4,
+      name: 'Spaghetti Bolognese',
+    },
+    {
+      id: 5,
+      imgUrl: '../images/food/plant_based_hamburger.png',
+      minutes: 60,
+      difficulty: 'Medium',
+      serves: 4,
+      name: 'Plant-based Hamburger',
+    },
+    {
+      id: 6,
+      imgUrl: '../images/food/strawberry_smoothie.png',
+      minutes: 10,
+      difficulty: 'Easy',
+      serves: 4,
+      name: 'Strawberry Smoothie',
+    },
   ];
 
   return (
@@ -68,7 +116,7 @@ function Profile() {
           {isConnect ? '✅' : '️❗️'} Connect to backend {!isConnect && 'failed'}
         </li>
       </ul>
-      <div className="xxl:w-1/5 xl:w-2/5 lg:w-3/5 mx-auto">
+      <div className="lg:w-3/5 mx-auto">
         <div className="w-full sm:w-3/4 flex-col sm:flex-row flex flex-wrap mx-auto">
           <div className="sm:w-1/2 w-full">
             <InfoCard title="Username's profile">
@@ -76,10 +124,22 @@ function Profile() {
                 <div className="flex-1">
                   <div className="font-bold mb-1">Info</div>
                   <div className="text-sm">
-                    <p>User ID: 0</p>
-                    <p>Recipes: 0</p>
-                    <p>Last Recipe: None</p>
-                    <p>Registered at: None</p>
+                    <p>
+                      <strong>User name: </strong>
+                      {userData.user_username}
+                    </p>
+                    <p>
+                      <strong>Recipes: </strong>
+                      {myRecipes.length}
+                    </p>
+                    <p>
+                      <strong>Last RecipeID: </strong>
+                      {myRecipes[0]}
+                    </p>
+                    <p>
+                      <strong>Registered at: </strong>
+                      {convertIsoTimestampToDate(userData.user_registration_date)}
+                    </p>
                   </div>
                 </div>
               </div>
