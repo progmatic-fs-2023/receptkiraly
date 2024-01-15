d6468b4bf5a24b1081b3f92ea0f0d653fbae7c37
MeetAlica <feher.ali.96@gmail.com>
2024-01-15 19:49:21 +0100

    Moving imageUpload multer to a middleware, and giving new name to the saved image

--- server/src/routes/recipes.route.js
+++ server/src/routes/recipes.route.js
@@ -1,19 +1,8 @@
 import express from 'express';
-import multer from 'multer';
+import { upload } from '../middlewares/imageUpload';
 import * as recipesController from '../controllers/recipes.controller';
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
 
 router.get('/', recipesController.list);
 router.get('/:id', recipesController.get);
