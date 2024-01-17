bde5851ebe62d8ad7ab83430ffe2f956d92128e9
kotaizsaklin <kotai.zsaklin@gmail.com>
2024-01-17 20:19:40 +0100

    DetailedRecipe véglegesítése

--- client/src/components/DetailedRecipe.jsx
+++ client/src/components/DetailedRecipe.jsx
@@ -44,70 +44,85 @@ function DetailedRecipe({ editMode }) {
 
   return (
     <section>
-      <form onSubmit="API CALL">
+      <form onSubmit="API CALL" className="flex flex-col flex-nowrap items-center">
         <div className="flex flex-col lg:flex-row">
           <div className="flex-1 flex flex-col lg:flex-row items-center justify-between lg:w-2/3 p-2">
             <div className="flex-1">
               <ImageUpload
-            editMode={editMode}
-            fileUpload={fileUpload}
-            setFileUpload={setFileUpload}
-            imgUrl={imgUrl}
-            setImgUrl={setImgUrl}
-            />
+                editMode={editMode}
+                fileUpload={fileUpload}
+                setFileUpload={setFileUpload}
+                imgUrl={imgUrl}
+                setImgUrl={setImgUrl}
+              />
             </div>
             <div className="flex-grow flex flex-wrap flex-col">
-            <RecipeTitle
-              editMode={editMode}
-              recipeTitle={recipeTitle}
-              setRecipeTitle={setRecipeTitle}
-            />
-            <Method editMode={editMode} description={description} setDescription={setDescription} />
-            <Ingredients
-              editMode={editMode}
-              ingredients={ingredients}
-              setIngredients={setIngredients}
-              newIngredient={newIngredient}
-              setNewIngredient={setNewIngredient}
-            />
+              <RecipeTitle
+                editMode={editMode}
+                recipeTitle={recipeTitle}
+                setRecipeTitle={setRecipeTitle}
+              />
+              <Method
+                editMode={editMode}
+                description={description}
+                setDescription={setDescription}
+              />
+              <Ingredients
+                editMode={editMode}
+                ingredients={ingredients}
+                setIngredients={setIngredients}
+                newIngredient={newIngredient}
+                setNewIngredient={setNewIngredient}
+              />
             </div>
           </div>
-          <div className="w-full lg:w-1/3 bg-gray-300 p-2 mt-4 lg:mt-0">
-          <RecipeMainCategory
+          <div className="w-full lg:w-1/3 p-2 mt-4 lg:mt-0">
+            <RecipeMainCategory
               editMode={editMode}
               selectedMainCategory={selectedMainCategory}
               setSelectedMainCategory={setSelectedMainCategory}
             />
             <RecipeCategory editMode={editMode} category={category} setCategory={setCategory} />
-          <IconContainer>
+            <IconContainer addClassName="flex-wrap flex-row items-center">
               {minutes ? (
-                <Icon imgUrl="/images/time-icon.svg" text={`${minutes} mins`} editMode={editMode} />
+                <Icon
+                  imgUrl="/images/time-icon.svg"
+                  text={`${minutes} mins`}
+                  editMode={editMode}
+                  addClassName="text-right w-20"
+                />
               ) : null}
               {difficulty ? (
-                <Icon imgUrl="/images/difficulty-icon.svg" text={difficulty} editMode={editMode} />
+                <Icon
+                  imgUrl="/images/difficulty-icon.svg"
+                  text={difficulty}
+                  editMode={editMode}
+                  addClassName="text-right w-20"
+                />
               ) : null}
               {serves ? (
                 <Icon
                   imgUrl="/images/serves-icon.svg"
                   text={serves.toString()}
                   editMode={editMode}
+                  addClassName="text-right w-20"
                 />
               ) : null}{' '}
             </IconContainer>
             <Labels
-                editMode={editMode}
-                selectedOptions={selectedOptions}
-                setSelectedOptions={setSelectedOptions}
-              />
+              editMode={editMode}
+              selectedOptions={selectedOptions}
+              setSelectedOptions={setSelectedOptions}
+            />
           </div>
         </div>
         {editMode ? (
-            <Button
-              type="submit"
-              text="Save"
-              className="flex mx-auto bg-amber-300 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg font-medium"
-            />
-          ) : null}
+          <Button
+            type="submit"
+            text="Save"
+            className="flex mx-auto bg-amber-300 border-0 py-2 px-8 focus:outline-none hover:bg-orange-600 rounded text-lg font-medium"
+          />
+        ) : null}
       </form>
     </section>
   );
--- client/src/components/IconContainer.jsx
+++ client/src/components/IconContainer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function IconContainer({ children, addClassName }) {
   return (
     <div className="hidden sm:block mt-4">
-      <div className={`flex sm:flex-wrap justify-center ${addClassName}`}>{children.map((child) => child)} </div>
+      <div className={`flex ${addClassName}`}>{children.map((child) => child)} </div>
     </div>
   );
 }
