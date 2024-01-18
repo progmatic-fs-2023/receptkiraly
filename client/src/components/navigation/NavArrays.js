4f188baf25c0148ab331730b1d8e0ca2228c4942
Gabor Jakobszen <jakobszen.gabor@gmail.com>
2024-01-18 18:55:49 +0100

    Create the arrays for the navigation.

---
+++ client/src/components/navigation/NavArrays.js
@@ -0,0 +1,36 @@
+export const meals = [
+  { key: 1, value: 'appetizers', text: 'Appetizers' },
+  { key: 2, value: 'breakfast', text: 'Breakfast' },
+  { key: 3, value: 'lunch', text: 'Lunch' },
+  { key: 4, value: 'dinner', text: 'Dinner' },
+];
+
+export const desserts = [
+  { key: 1, value: 'cakes', text: 'Cakes' },
+  { key: 2, value: 'frozen_desserts', text: 'Frozen desserts' },
+  { key: 3, value: 'Chocolate_and_candies', text: 'Chocolate and candies' },
+  { key: 4, value: 'fruit_desserts', text: 'Fruit desserts' },
+];
+
+export const beverages = [
+  { key: 1, value: 'hot_drinks', text: 'Hot drinks' },
+  { key: 2, value: 'cold_drinks', text: 'cold drinks' },
+  { key: 3, value: 'soft_drinks', text: 'soft drinks' },
+  { key: 4, value: 'juices', text: 'juices' },
+  { key: 5, value: 'smoothies', text: 'smoothies' },
+];
+
+export const chefsProf = [
+  { key: 1, value: 'executive', text: 'Hot drinks' },
+  { key: 2, value: 'sous', text: 'cold drinks' },
+  { key: 3, value: 'pastry', text: 'soft drinks' },
+  { key: 4, value: 'cold_kitchen', text: 'juices' },
+  { key: 5, value: 'saucier', text: 'smoothies' },
+];
+
+export const chefsAmateur = [
+  { key: 1, value: 'dinner', text: 'Dinner' },
+  { key: 2, value: 'soup', text: 'Soup' },
+  { key: 3, value: 'italian', text: 'Italian' },
+  { key: 4, value: 'fast', text: 'Fast' },
+];
