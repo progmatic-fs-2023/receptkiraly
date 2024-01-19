7858b60fed2c0688d5842024df017bea18c6776a
katonabalint13 <katonabalint13@gmail.com>
2024-01-19 10:21:53 +0100

    About page changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -39,7 +39,7 @@ function About() {
       <section className="text-gray-600 body-font relative ml-32 mr-32">
         <div className="container  py-24 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            <iframe
+            <div
               width="100%"
               height="100%"
               className="absolute inset-0"
