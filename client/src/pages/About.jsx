9d4f6f0c895dbad0b819ab834d70ce8739ff49ef
katonabalint13 <katonabalint13@gmail.com>
2024-01-19 10:40:22 +0100

    About map bugfix

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -39,11 +39,13 @@ function About() {
       <section className="text-gray-600 body-font relative ml-32 mr-32">
         <div className="container  py-24 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            <input
+            <iframe
+              width="100%"
+              height="100%"
               className="absolute inset-0"
+              title="map"
               src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
             />
-
             <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md mx-6">
               <div className="lg:w-1/2 px-6">
                 <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
