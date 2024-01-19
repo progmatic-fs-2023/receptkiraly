427b8ee1409f6a1a621db03f39981c03edfe74ca
katonabalint13 <katonabalint13@gmail.com>
2024-01-19 11:06:15 +0100

    Delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,15 @@
 import { Route, Routes } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
-import './components/RecipePostsCard.css';
-import './components/DailyRecipeCard.css';
+
 /* Navbar pages */
 import SearchRecipes from './pages/SearchRecipes';
 import Home from './pages/Home';
-import RecipesFeed from './pages/RecipesFeed';
-import Battle from './pages/Battle';
+
 import Registration from './pages/Registration';
 import NoPage from './pages/NoPages';
 import Profile from './pages/Profile';
-import Rankings from './pages/Rankings';
-import NewRecipe from './pages/NewRecipe';
+
 /* Template */
 import Banner from './components/Banner';
 import Fallback from './components/Fallback';
@@ -29,26 +26,33 @@ function App() {
         <div className="flex justify-center">
           <NavigationBar />
         </div>
-        </header>
-        <img src="/images/meal.svg" alt="Meal Icon" className="absolute left-0 top-0 z-0 hidden sm:block"/>
-        <img src="/images/stirring.svg" alt="Meal Icon" className="absolute right-0 transform scale-y-[-1] top-0 z-0 hidden sm:block"/>
-        <main className="py-10">
+      </header>
+      <img
+        src="/images/meal.svg"
+        alt="Meal Icon"
+        className="absolute left-0 top-0 z-0 hidden sm:block"
+      />
+      <img
+        src="/images/stirring.svg"
+        alt="Meal Icon"
+        className="absolute right-0 transform scale-y-[-1] top-0 z-0 hidden sm:block"
+      />
+      <main>
         <ErrorBoundary FallbackComponent={Fallback}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/recipesfeed" element={<RecipesFeed />} />
-              <Route path="/search" element={<SearchRecipes />} />
-              <Route path="/battle" element={<Battle />} />
-              <Route path="/register" element={<Registration />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/rankings" element={<Rankings />} />
-              <Route path="/postrecipe" element={<NewRecipe />} />
-              <Route path="/*" element={<NoPage />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/search" element={<SearchRecipes />} />
+
+            <Route path="/register" element={<Registration />} />
+            <Route path="/profile" element={<Profile />} />
+
+            <Route path="/*" element={<NoPage />} />
+          </Routes>
         </ErrorBoundary>
-        </main>
-        <footer className="relative z-0">
-      <Footer />
+      </main>
+      <footer className="relative z-50">
+        <Footer />
       </footer>
     </div>
   );
