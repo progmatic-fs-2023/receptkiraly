19c851c8ff03612c597d331233465e880a40858a
MeetAlica <feher.ali.96@gmail.com>
2024-01-20 19:57:10 +0100

    Fixed prettier issue

--- server/src/services/login.services.js
+++ server/src/services/login.services.js
@@ -2,10 +2,10 @@ import jwt from 'jsonwebtoken';
 import * as db from './db.service';
 
 export const loginUser = async (username, password) => {
-  const result = await db.query(
-    'SELECT * FROM users WHERE username = $1 AND password_hash = $2',
-    [username, password],
-  );
+  const result = await db.query('SELECT * FROM users WHERE username = $1 AND password_hash = $2', [
+    username,
+    password,
+  ]);
   const user = result.rows[0];
 
   if (user) {
