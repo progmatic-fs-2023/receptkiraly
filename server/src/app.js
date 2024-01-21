88116d8e2ae970bcfbf3aa4a2a8d72d6f1ba38d6
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-01-21 09:35:42 +0100

    Added authenticate middleware, ripes and users routes are now bound to authentication, wihtout login the response is 403 unauthorized, --> req.userID now contains the id of the user who is logged in and is sending the request

--- server/src/app.js
+++ server/src/app.js
@@ -7,18 +7,21 @@ import apiRouter from './routes/api.router';
 import loginRouter from './routes/login.router';
 import registerRouter from './routes/register.route';
 import userRouter from './routes/user.router';
+import cookieParser from 'cookie-parser';
+import authenticate from './middlewares/authenticate.middleware';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(logger);
 
-app.use('/api/recipes', recipeRouter);
+app.use('/api/recipes', authenticate, recipeRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/register', registerRouter);
-app.use('/api/user', userRouter);
+app.use('/api/user', authenticate, userRouter);
 app.use('/api', apiRouter);
 app.use(express.static('dist'));
 
