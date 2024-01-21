e00cf49bd2c2a8e691d81f73f8a57cfd83f9a60b
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-01-21 08:16:22 +0100

    Added context.provider --> providing the value of a new state in App jsx to all components wrapped

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 'use client';
 
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
+import LoginContext from './contexts/LoginContext';
+import LoginDispatchContext from './contexts/LoginDispatchContext';
 import './App.css';
 import './components/RecipePostsCard.css';
 import './components/DailyRecipeCard.css';
@@ -22,42 +25,47 @@ import NavigationBar from './components/navigation/NavigationBar';
 import Footer from './components/Footer';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-      <header className="container mx-auto w-full">
-        <Banner />
-        <div className="flex justify-center">
-          <NavigationBar />
-        </div>
-      </header>
-      <img
-        src="/images/meal.svg"
-        alt="Meal Icon"
-        className="absolute left-0 top-0 z-0 hidden sm:block"
-      />
-      <img
-        src="/images/stirring.svg"
-        alt="Meal Icon"
-        className="absolute right-0 transform scale-y-[-1] top-0 z-0 hidden sm:block"
-      />
-      <main className="py-10">
-        <ErrorBoundary FallbackComponent={Fallback}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipesfeed" element={<RecipesFeed />} />
-            <Route path="/search" element={<SearchRecipes />} />
-            <Route path="/battle" element={<Battle />} />
-            <Route path="/register" element={<Registration />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/rankings" element={<Rankings />} />
-            <Route path="/postrecipe" element={<NewRecipe />} />
-            <Route path="/*" element={<NoPage />} />
-          </Routes>
-        </ErrorBoundary>
-      </main>
-      <footer className="relative z-0">
-        <Footer />
-      </footer>
+      <LoginContext.Provider value={isLoggedIn}>
+        <LoginDispatchContext.Provider value={setIsLoggedIn}>
+          <header className="container mx-auto w-full">
+            <Banner />
+            <div className="flex justify-center">
+              <NavigationBar />
+            </div>
+          </header>
+          <img
+            src="/images/meal.svg"
+            alt="Meal Icon"
+            className="absolute left-0 top-0 z-0 hidden sm:block"
+          />
+          <img
+            src="/images/stirring.svg"
+            alt="Meal Icon"
+            className="absolute right-0 transform scale-y-[-1] top-0 z-0 hidden sm:block"
+          />
+          <main className="py-10">
+            <ErrorBoundary FallbackComponent={Fallback}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/recipesfeed" element={<RecipesFeed />} />
+                <Route path="/search" element={<SearchRecipes />} />
+                <Route path="/battle" element={<Battle />} />
+                <Route path="/register" element={<Registration />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/rankings" element={<Rankings />} />
+                <Route path="/postrecipe" element={<NewRecipe />} />
+                <Route path="/*" element={<NoPage />} />
+              </Routes>
+            </ErrorBoundary>
+          </main>
+          <footer className="relative z-0">
+            <Footer />
+          </footer>
+        </LoginDispatchContext.Provider>
+      </LoginContext.Provider>
     </div>
   );
 }
