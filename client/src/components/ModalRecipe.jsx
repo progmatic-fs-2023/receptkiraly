c96b2566642a161daae7e43512fad0624815ec22
jakobszengabor <jakobszen.gabor@gmail.com>
2024-01-22 21:25:46 +0100

    Minor changes on ModalRecipe

--- client/src/components/ModalRecipe.jsx
+++ client/src/components/ModalRecipe.jsx
@@ -3,31 +3,33 @@ import PropTypes from 'prop-types';
 export default function Modal({ children, title, close, addClassName }) {
   return (
     <>
-      <div className="fixed inset-0 z-50 overflow-hidden flex justify-center items-center">
-        <div className={`relative overflow-y-auto ${addClassName}`}>
-          <div className="flex flex-col h-full bg-white rounded-lg shadow-lg overflow-hidden">
-            <div className="flex-shrink-0">
-              <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                <h3 className="text-3xl font-semibold">{title}</h3>
-                <button
-                  type="button"
-                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                  onClick={close}
-                >
-                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                    ×
-                  </span>
-                </button>
-              </div>
-            </div>
-            <div className="flex-1 overflow-y-auto p-6">
-              <div className="my-4 text-blueGray-500 text-lg leading-relaxed text-justify">
-                {children}
-              </div>
-            </div>
-            <div className="flex-shrink-0 p-6 border-t border-solid border-blueGray-200 rounded-b">
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none top-0">
+        <div className={`relative my-6 mx-auto max-h-screen overflow-y-auto ${addClassName}`}>
+          {/* content */}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/* header */}
+            <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-3xl font-semibold">{title}</h3>
               <button
-                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none ease-linear transition-all duration-150"
+                type="button"
+                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                onClick={close}
+              >
+                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                  ×
+                </span>
+              </button>
+            </div>
+            {/* body */}
+            <div className="relative p-6 flex-auto">
+              <p className="my-4 text-blueGray-500 text-lg leading-relaxed text-justify">
+                {children}
+              </p>
+            </div>
+            {/* footer */}
+            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+              <button
+                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
                 onClick={close}
               >
@@ -37,7 +39,7 @@ export default function Modal({ children, title, close, addClassName }) {
           </div>
         </div>
       </div>
-      <div className="fixed inset-0 z-40 bg-black opacity-25" />
+      <div className="opacity-25 fixed inset-0 z-40 bg-black" />
     </>
   );
 }
@@ -48,7 +50,6 @@ Modal.propTypes = {
   close: PropTypes.func.isRequired,
   addClassName: PropTypes.string,
 };
-
 Modal.defaultProps = {
   addClassName: '',
 };
