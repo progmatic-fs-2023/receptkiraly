b5170532c0345cea5c6ba1f37df6a5afe5df309d
Mate Kortvelyessy <kortvelyessymate16@gmail.com>
2024-01-22 20:45:12 +0100

    Added new preprocess function to helpers which will group the database query based on ID and return a new array where the label_name is an array

--- server/src/utils/helpers.js
+++ server/src/utils/helpers.js
@@ -1,3 +1,33 @@
 export function transformArrayToIntegers(arrayOfObjects, key) {
   return arrayOfObjects.map(obj => parseInt(obj[key], 10));
 }
+
+//groups an array and returns an object, each key of the object will represent a group
+function groupBy(array, keyOrIterator) {
+  let iterator, key;
+
+  // use the function passed in, or create one
+  if (typeof keyOrIterator !== 'function') {
+    key = String(keyOrIterator);
+    iterator = function (item) {
+      return item[key];
+    };
+  } else {
+    iterator = keyOrIterator;
+  }
+
+  return array.reduce(function (memo, item) {
+    let key = iterator(item);
+    memo[key] = memo[key] || [];
+    memo[key].push(item);
+    return memo;
+  }, {});
+}
+
+export const prepocessRecipe = Object.values(groupBy(arr, elem => elem.id)).map(arr => {
+  const labels = arr.reduce((acc, curr) => {
+    curr.label_name ? acc.push(curr.label_name) : null;
+    return acc;
+  }, []);
+  return { ...arr[0], label_name: labels };
+});
