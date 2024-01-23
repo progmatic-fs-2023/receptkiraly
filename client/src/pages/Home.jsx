4998c7e544584c5bba9e4bfeb16f088e8c027ac0
katonabalint13 <katonabalint13@gmail.com>
2024-01-23 10:25:04 +0100

    Add useContent to Modals

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -32,7 +32,6 @@ function Home() {
   const editMode = true;
   const isAuthenticated = useContext(LoginContext);
   const setIsAuthenticated = useContext(LoginDispatchContext);
-  // const [isLoggedIn, setIsLoggedIn] = useState(false); // Here
   const [isCreateRecipe, setISCreateRecipe] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
 
@@ -45,7 +44,7 @@ function Home() {
   };
 
   const handleLogin = () => {
-    setIsAuthenticated;
+    setIsAuthenticated(true);
     setShowLoginModal(false);
   };
 
@@ -153,10 +152,10 @@ function Home() {
         </div>
       </div>
 
-      {/* <div>
+      <div>
         <div>
           <SwiperComponent title="Most Popular">
-            {latestRecipe.map(() => (
+            {latestRecipes.map(() => (
               <RecipeCard
                 id={7}
                 imgUrl="https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_2480_1860.webp 2480w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_2074_1556.webp 2074w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_1735_1301.webp 1735w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_1452_1089.webp 1452w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_1215_911.webp 1215w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_1016_762.webp 1016w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_850_638.webp 850w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_711_533.webp 711w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_595_446.webp 595w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_498_374.webp 498w, https://d2vsf1hynzxim7.cloudfront.net/production/media/23976/responsive-images/air-fryer-prawns___default_416_312.webp 416w"
@@ -168,7 +167,7 @@ function Home() {
             ))}
           </SwiperComponent>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
