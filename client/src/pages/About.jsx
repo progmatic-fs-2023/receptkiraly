19ac215940682ab7aa4d0f0782eeabab13edbec0
katonabalint13 <katonabalint13@gmail.com>
2024-01-24 19:29:42 +0100

    Add About page path

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom';
-import AboutIMG from '../assets/Aboutpageimg.png';
+import AboutIMG from '../assets/Aboutimg.png';
 
 function About() {
   return (
-    <div>
+    <div className="">
       <Link to="/" className="hover:underline mb-6 relative z-10">
         <div className="ml-32">Home</div>
       </Link>
@@ -36,7 +36,7 @@ function About() {
           </p>
         </div>
       </div>
-      <section className="text-gray-600 body-font relative ml-32 mr-32">
+      <section className="text-gray-600 body-font ">
         <div className="container  py-24 mx-auto flex sm:flex-nowrap flex-wrap">
           <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
             <iframe
@@ -67,7 +67,7 @@ function About() {
               </div>
             </div>
           </div>
-          <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <div className="lg:w-1/4 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
             <p className="leading-relaxed mb-5 text-gray-600">
               Post-ironic portland shabby chic echo park, banjo fashion axe
