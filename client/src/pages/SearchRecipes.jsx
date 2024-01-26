a3d0c3dfb13441542cb891eab322d3f7aa7fad02
jakobszengabor <jakobszen.gabor@gmail.com>
2024-01-27 00:17:50 +0100

    Add custom Hook for SearchRecipes.jsx.

--- client/src/pages/SearchRecipes.jsx
+++ client/src/pages/SearchRecipes.jsx
@@ -5,68 +5,12 @@ import RecipeGrid from '../components/RecipeGrid';
 import Modal from '../components/Modal';
 import DetailedRecipe from '../components/DetailedRecipe';
 import { API_URL, HOST_PORT_URL } from '../constants';
+import useRecipeCardModal from '../hooks/useRecipeCardModal';
 
 function SearchRecipes() {
   const [recipesData, setRecipesData] = useState([]);
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
-  // States for one recipe
-  const [recipeTitle, setRecipeTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [imgUrl, setImgUrl] = useState();
-  const [minutes, setMinutes] = useState('');
-  const [difficulty, setDifficulty] = useState('');
-  const [serves, setServes] = useState('');
-  const [category, setCategory] = useState('');
-  const [selectedMainCategory, setSelectedMainCategory] = useState('');
-  const [selectedOptions, setSelectedOptions] = useState([]);
-  const [ingredients, setIngredients] = useState([]);
-  const [, setErrorMessage] = useState();
-
-  const stateObject = {
-    title: {
-      value: recipeTitle,
-      setter: setRecipeTitle,
-    },
-    description: {
-      value: description,
-      setter: setDescription,
-    },
-    image: {
-      value: imgUrl,
-      setter: setImgUrl,
-    },
-    time: {
-      value: minutes,
-      setter: setMinutes,
-    },
-    difficulty: {
-      value: difficulty,
-      setter: setDifficulty,
-    },
-    serves: {
-      value: serves,
-      setter: setServes,
-    },
-    category: {
-      value: category,
-      setter: setCategory,
-    },
-    mainCategory: {
-      value: selectedMainCategory,
-      setter: setSelectedMainCategory,
-    },
-    labels: {
-      value: selectedOptions,
-      setter: setSelectedOptions,
-    },
-    ingredients: {
-      value: ingredients,
-      setter: setIngredients,
-    },
-  };
-  // ------------------- End of the Detailed Recipe States
+  const { stateObject, closeModal, openModal, isModalOpen, selectedRecipe } = useRecipeCardModal();
 
   useEffect(() => {
     const apiUrl = `${API_URL}/search${window.location.search}`;
@@ -80,37 +24,6 @@ function SearchRecipes() {
         // console.log(recipes);
       });
   }, []);
-
-  // Modal functions
-  const openModal = async (id) => {
-    setSelectedRecipe(id);
-    setModalOpen(true);
-
-    try {
-      // console.log(`Recipe ID search URL: http://localhost:3000/api/recipes/${id}`);
-      const response = await fetch(`${API_URL}/recipes/${id}`);
-      const recipe = await response.json();
-
-      setRecipeTitle(recipe.recipe_name);
-      setDescription(recipe.description);
-      setImgUrl(recipe.img);
-      setMinutes(recipe.time_minutes);
-      setDifficulty(recipe.difficulty_level);
-      setServes(recipe.serve_count);
-      setCategory(recipe.category_name);
-      setSelectedMainCategory(recipe.main_category_name);
-      setSelectedOptions(recipe.label_name);
-
-      // console.log(recipe);
-    } catch (error) {
-      setErrorMessage(error);
-    }
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-  // ---------------------------
 
   return (
     <section className="container mx-auto my-2">
