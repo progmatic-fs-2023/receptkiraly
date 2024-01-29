a27395f37ffb3fe33e2459600abda261dfb0f7d5
MeetAlica <feher.ali.96@gmail.com>
2024-01-29 20:49:34 +0100

    emptyFields logic moved to detailedRecipe, and using detailedRecipe instead of PostNewRecipe

--- client/src/pages/NewRecipe.jsx
+++ client/src/pages/NewRecipe.jsx
@@ -1,14 +1,12 @@
-import PostNewRecipe from '../components/PostNewRecipe';
 import useRecipeCardModal from '../hooks/useRecipeCardModal';
+import DetailedRecipe from '../components/DetailedRecipe';
 
 function NewRecipe() {
-  const editMode = true;
-
-  const { stateObject } = useRecipeCardModal();
+  const { stateObject, closeModal } = useRecipeCardModal();
 
   return (
     <section className="container mx-auto my-2">
-      <PostNewRecipe editMode={editMode} stateObject={stateObject} />
+      <DetailedRecipe editMode stateObject={stateObject} closeModal={closeModal} />
     </section>
   );
 }
