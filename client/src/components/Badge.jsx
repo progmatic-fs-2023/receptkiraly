98f26a24ef852e306d86d6e773876a014b5de685
kotaizsaklin <kotai.zsaklin@gmail.com>
2024-01-31 17:32:55 +0100

    prettier

--- client/src/components/Badge.jsx
+++ client/src/components/Badge.jsx
@@ -1,39 +1,41 @@
 import PropTypes from 'prop-types';
 
 function Badge({ label }) {
-    const getColorClass = () => {
-        switch (label) {
-            case 'vegan':
-                return 'bg-green-100 text-green-800';
-              case 'vegetarian':
-                return 'bg-lime-100 text-lime-800';
-              case 'nut-free':
-                return 'bg-amber-500 text-yellow-800';
-              case 'egg-free':
-                return 'bg-orange-500 text-yellow-50';
-              case 'dairy-free':
-                return 'bg-amber-100 text-amber-500';
-              case 'gluten-free':
-                return 'bg-orange-100 text-orange-800';
-              case 'low-carb':
-                return 'bg-blue-100 text-blue-800';
-              case 'spicy':
-                return 'bg-red-400 text-red-900';
-              case 'alcoholic':
-                return 'bg-gray-500 text-gray-100';
-              case 'non-alcoholic':
-                return 'bg-teal-100 text-teal-800';
-              case 'seafood':
-                return 'bg-aqua-100 text-aqua-800';
-              default:
-                return 'bg-gray-100 text-gray-800';
-        }
-      };
+  const getColorClass = () => {
+    switch (label) {
+      case 'vegan':
+        return 'bg-green-100 text-green-800';
+      case 'vegetarian':
+        return 'bg-lime-100 text-lime-800';
+      case 'nut-free':
+        return 'bg-amber-500 text-yellow-800';
+      case 'egg-free':
+        return 'bg-orange-500 text-orange-50';
+      case 'dairy-free':
+        return 'bg-amber-100 text-amber-800';
+      case 'gluten-free':
+        return 'bg-orange-100 text-orange-800';
+      case 'low-carb':
+        return 'bg-blue-100 text-blue-800';
+      case 'spicy':
+        return 'bg-red-400 text-red-900';
+      case 'alcoholic':
+        return 'bg-gray-500 text-gray-100';
+      case 'non-alcoholic':
+        return 'bg-teal-100 text-teal-800';
+      case 'seafood':
+        return 'bg-aqua-100 text-aqua-800';
+      default:
+        return 'bg-gray-100 text-gray-800';
+    }
+  };
 
-      const colorClass = getColorClass();
+  const colorClass = getColorClass();
 
   return (
-    <span className={`${colorClass} text-sm font-medium me-2 px-2.5 py-0.5 rounded p-1 m-1`}>#{label}</span>
+    <span className={`${colorClass} text-sm font-medium me-2 px-2.5 py-0.5 rounded m-1`}>
+      #{label}
+    </span>
   );
 }
 
