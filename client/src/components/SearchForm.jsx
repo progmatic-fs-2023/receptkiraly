47482901585cd4cc5d879675782288d32f42c17d
MeetAlica <feher.ali.96@gmail.com>
2024-01-31 18:17:00 +0100

    Removed selected and added defaultValue

--- client/src/components/SearchForm.jsx
+++ client/src/components/SearchForm.jsx
@@ -156,12 +156,13 @@ function SearchFilter({ setRecipesData, setErrorMessage }) {
                   <label htmlFor="mealType" className="block mb-2 text-sm">
                     Type
                     <select
+                      defaultValue={<option value="">All</option>}
                       id="mealType"
                       className="bg-white border-2 border-gray-300 rounded w-full py-2"
                       value={selectedType}
                       onChange={(e) => setSelectedType(e.target.value)}
                     >
-                      <option selected>All</option>
+                      <option value="">All</option>
                       <option value="meals">Meal</option>
                       <option value="desserts">Dessert</option>
                       <option value="beverages">Beverage</option>
